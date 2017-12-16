c4bf1d00e8c9dcac71480d35a2845476c23e1deb
termik88 <dirkser@mail.ru>
2017-12-16 22:30:31 +0500

    End

--- js/dialog.js
+++ js/dialog.js
@@ -54,11 +54,13 @@
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
     document.querySelector('.setup-similar').classList.remove('hidden');
+    setup.removeAttribute('style'); /* Обнуление локации окна */
   };
 
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.removeAttribute('style'); /* Обнуление локации окна */
   };
 
   setupOpen.addEventListener('click', function () {
@@ -110,6 +112,94 @@
 
   setupFireballWrap.addEventListener('click', function () {
     setupFireballWrap.style.background = randomNonRepeatingColor(window.PARAMETERS_MAGES.FIREBALL_COLORS, rgbToHex(setupFireballWrap.style.background));
+  });
+
+  /* Перетаскивание */
+  var dialogHandle = setup.querySelector('.setup-user-pic');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      if (moveEvt.clientY >= 0) {
+        if (moveEvt.clientX >= 0) {
+          var shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+          };
+
+          startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+          };
+
+          setup.style.top = (setup.offsetTop - shift.y) + 'px';
+          setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+        }
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  /* Перетаскивание звездочки */
+  var shopElement = setup.querySelector('.setup-artifacts-shop');
+  var artifactsElement = setup.querySelector('.setup-artifacts');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setDate('text/plain', evt.target.alt);
+    }
+  });
+
+  shopElement.addEventListener('drag', function (evt) {
+    artifactsElement.style.outline = '2px dashed red';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    artifactsElement.style.outline = '';
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = '2px dashed red';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragend', function (evt) {
+    artifactsElement.style.outline = '';
+    evt.preventDefault();
   });
 
 })();
