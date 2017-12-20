de10e0bbc5cf541d975a675f77b9dd2cc951d685
termik88 <dirkser@mail.ru>
2017-12-20 20:44:21 +0500

    server work

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,115 @@
 (function () {
   var NUMBER_WIZARD = 4;
 
+  var setup = document.querySelector('.setup');
+
+  /* Открытие/закрытие окна настроек чара */
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+
+  var onPopupEscPress = function (evt) {
+    if (!evt.target.classList.contains('setup-user-name')) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+    setup.removeAttribute('style'); /* Обнуление локации окна */
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+    setup.removeAttribute('style'); /* Обнуление локации окна */
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  /* Валидация окна настроек*/
+  var userNameInput = setup.querySelector('.setup-user-name');
+
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-ч символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-ч символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  /* Перетаскивание звездочки */
+  var shopElement = setup.querySelector('.setup-artifacts-shop');
+  var artifactsElement = setup.querySelector('.setup-artifacts');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setDate('text/plain', evt.target.alt);
+    }
+  });
+
+  shopElement.addEventListener('drag', function (evt) {
+    artifactsElement.style.outline = '2px dashed red';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    artifactsElement.style.outline = '';
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = '2px dashed red';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragend', function (evt) {
+    artifactsElement.style.outline = '';
+    evt.preventDefault();
+  });
+
   /* Выбор случайного цвета */
   window.colorizeElement = function (element, colors, currColor, nameFunction) {
     var randomNonRepeatingColor = function (array, except) {
@@ -41,31 +150,54 @@
     return hex;
   };
 
+  /* Функция полуения данных с сервера */
+  var similarListElement = setup.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
   var renderWizard = function (wizard) {
-    var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
     return wizardElement;
   };
 
-  var cloneWizard = function () {
-    var similarListElement = document.querySelector('.setup-similar-list');
+  var successHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
-    var wizards = [];
 
     for (var i = 0; i < NUMBER_WIZARD; i++) {
-      wizards[i] = {
-        name: window.PARAMETERS_MAGES.NAMES[window.util.getRandomIndex(window.PARAMETERS_MAGES.NAMES)] + ' ' + window.PARAMETERS_MAGES.SURNAMES[window.util.getRandomIndex(window.PARAMETERS_MAGES.SURNAMES)],
-        coatColor: window.PARAMETERS_MAGES.COAT_COLORS[window.util.getRandomIndex(window.PARAMETERS_MAGES.COAT_COLORS)],
-        eyesColor: window.PARAMETERS_MAGES.EYES_COLORS[window.util.getRandomIndex(window.PARAMETERS_MAGES.EYES_COLORS)]
-      };
       fragment.appendChild(renderWizard(wizards[i]));
     }
     similarListElement.appendChild(fragment);
+
+    setup.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  cloneWizard();
+  /* Обработчик ошибок для работы с сервером*/
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
+  /* Функция отправки формы на сервер */
+  var formSetup = setup.querySelector('.setup-wizard-form');
+
+  formSetup.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(formSetup), function (response) {
+      setup.classList.add('hidden');
+    }, errorHandler);
+    evt.preventDefault();
+  });
+
 })();
